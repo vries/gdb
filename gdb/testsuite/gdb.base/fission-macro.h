24ea485f68ba7cab07f04bccc485256ec0a338dd
Tom de Vries <tdevries@suse.de>
2024-05-18 16:21:13 +0200

    [gdb/testsuite] Extend gdb.base/fission-macro.exp
    Add a header file to test-case gdb.base/fission-macro.exp, and check that the
    macros defined there are properly listed.
    
    Tested on x86_64-linux.
    
    Tested test-case using a current gcc trunk build, and gcc 14.

--- gdb/testsuite/gdb.base/fission-macro.h
+++ gdb/testsuite/gdb.base/fission-macro.h
@@ -15,15 +15,5 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
-#define FIRST 1
-#define SECOND 2
-
-#include "fission-macro.h"
-
-extern int foo (void);
-
-int
-main (void)
-{
-  return foo ();
-}
+#define FIFTH 5
+#define SIXTH 6
