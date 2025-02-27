bminor/binutils-gdb

commit e2a1578868a2a681fbafd6c56b694cfe9410b8d4

gdb: link breakpoints with intrusive_list
Change-Id: I043d8d6f3dd864d80d5088f6ffc2c098337249ea
Reviewed-By: Andrew Burgess <aburgess@redhat.com>

gdb/breakpoint.c
@@ -614,14 +614,16 @@ bool target_exact_watchpoints = false;
 
 /* Chains of all breakpoints defined.  */
 
-static struct breakpoint *breakpoint_chain;
+static intrusive_list<breakpoint> breakpoint_chain;
 
 /* See breakpoint.h.  */
 
 breakpoint_range
 all_breakpoints ()
 {
-  return breakpoint_range (breakpoint_chain);
+  return breakpoint_range
+    (breakpoint_pointer_iterator (breakpoint_chain.begin ()),
+     breakpoint_pointer_iterator (breakpoint_chain.end ()));
 }
 
 /* See breakpoint.h.  */
@@ -637,7 +639,9 @@ all_breakpoints_safe ()
 tracepoint_range
 all_tracepoints ()
 {
-  return tracepoint_range (breakpoint_chain);
+  return tracepoint_range
+    (tracepoint_iterator (breakpoint_pointer_iterator (breakpoint_chain.begin ())),
+     tracepoint_iterator (breakpoint_pointer_iterator (breakpoint_chain.end ())));
 }
 
 /* Array is sorted by bp_location_is_less_than - primarily by the ADDRESS.  */
@@ -7428,23 +7432,12 @@ decref_bp_location (struct bp_location **blp)
 static breakpoint *
 add_to_breakpoint_chain (std::unique_ptr<breakpoint> &&b)
 {
-  struct breakpoint *b1;
-  struct breakpoint *result = b.get ();
-
   /* Add this breakpoint to the end of the chain so that a list of
      breakpoints will come out in order of increasing numbers.  */
 
-  b1 = breakpoint_chain;
-  if (b1 == 0)
-    breakpoint_chain = b.release ();
-  else
-    {
-      while (b1->next)
-	b1 = b1->next;
-      b1->next = b.release ();
-    }
+  breakpoint_chain.push_back (*b.release ());
 
-  return result;
+  return &breakpoint_chain.back ();
 }
 
 /* Initialize loc->function_name.  */
@@ -12372,15 +12365,7 @@ delete_breakpoint (struct breakpoint *bpt)
   if (bpt->number)
     gdb::observers::breakpoint_deleted.notify (bpt);
 
-  if (breakpoint_chain == bpt)
-    breakpoint_chain = bpt->next;
-
-  for (breakpoint *b : all_breakpoints ())
-    if (b->next == bpt)
-      {
-	b->next = bpt->next;
-	break;
-      }
+  breakpoint_chain.erase (breakpoint_chain.iterator_to (*bpt));
 
   /* Be sure no bpstat's are pointing at the breakpoint after it's
      been freed.  */
@@ -14442,7 +14427,6 @@ _initialize_breakpoint ()
   gdb::observers::memory_changed.attach (invalidate_bp_value_on_memory_change,
 					 "breakpoint");
 
-  breakpoint_chain = 0;
   /* Don't bother to call set_breakpoint_count.  $bpnum isn't useful
      before a breakpoint is set.  */
   breakpoint_count = 0;

gdb/breakpoint.h
@@ -616,7 +616,7 @@ using bp_location_range = iterator_range<bp_location_iterator>;
 
 /* Abstract base class representing all kinds of breakpoints.  */
 
-struct breakpoint
+struct breakpoint : public intrusive_list_node<breakpoint>
 {
   breakpoint (struct gdbarch *gdbarch_, enum bptype bptype,
 	      bool temp = true, const char *cond_string = nullptr);
@@ -791,7 +791,6 @@ struct breakpoint
     /* Nothing to do.  */
   }
 
-  breakpoint *next = NULL;
   /* Type of breakpoint.  */
   bptype type = bp_none;
   /* Zero means disabled; remember the info but don't break here.  */
@@ -1894,11 +1893,15 @@ public:
 
 /* Breakpoint linked list iterator.  */
 
-using breakpoint_iterator = next_iterator<breakpoint>;
+using breakpoint_list = intrusive_list<breakpoint>;
+
+using breakpoint_iterator = breakpoint_list::iterator;
+
+using breakpoint_pointer_iterator = reference_to_pointer_iterator<breakpoint_iterator>;
 
 /* Breakpoint linked list range.  */
 
-using breakpoint_range = iterator_range<breakpoint_iterator>;
+using breakpoint_range = iterator_range<breakpoint_pointer_iterator>;
 
 /* Return a range to iterate over all breakpoints.  */
 
@@ -1925,7 +1928,7 @@ struct tracepoint_filter
 /* Breakpoint linked list iterator, filtering to only keep tracepoints.  */
 
 using tracepoint_iterator
-  = filtered_iterator<breakpoint_iterator, tracepoint_filter>;
+  = filtered_iterator<breakpoint_pointer_iterator, tracepoint_filter>;
 
 /* Breakpoint linked list range, filtering to only keep tracepoints.  */
 

vries/gdb

commit 7d98fe729b0e78b57807430d57e2303d50965964

[gdb] Fix assert in delete_breakpoint
On a pinebook (aarch64 with 64-bit kernel and 32-bit userland) with test-case
gdb.base/gdb-sigterm.exp I run into:
...
intrusive_list.h:458: internal-error: erase_element: \
  Assertion `elem_node->prev != INTRUSIVE_LIST_UNLINKED_VALUE' failed.^M
...
which happens while executing this statement in delete_breakpoint:
...
  breakpoint_chain.erase (breakpoint_chain.iterator_to (*bpt));
...

The following events lead to the assertion failure:
- a single-step breakpoint is hit,
- delete_just_stopped_threads_single_step_breakpoints is called,
- during delete_just_stopped_threads_single_step_breakpoints,
  delete_breakpoint is called,
- breakpoint_chain.erase is called,
- gdb is interrupted by SIGTERM before finishing delete_breakpoint.
  The interrupt happens due to a QUIT during a target_write_with_progress
  call, which is called during default_memory_remove_breakpoint,
- the SIGTERM triggers a SCOPE_EXIT cleanup, calling
  delete_just_stopped_threads_infrun_breakpoints which ends up
  calling delete_breakpoint again for the same breakpoint, and
- breakpoint_chain.erase is called the second time, and the assert triggers.

There is an ad-hoc reentrancy control mechanism in delete_breakpoint:
...
void
delete_breakpoint (struct breakpoint *bpt)
{
  ....
  if (bpt->type == bp_none)
    return;
  ...
  bpt->type = bp_none;
  ...
}
...
which partitions up the function into three parts:
- an initial part that is always executed,
- a second part that can be executed more than once per breakpoint, and
- a third part that can be executed only once per breakpoint.

One way of looking at the assertion failure is that commit e2a1578868a ("gdb:
link breakpoints with intrusive_list") introduced code in the second part that
cannot be executed more than once, which can be fixed by guarding the
breakpoint_chain.erase call with is_linked.

Another way of looking at it is to ask the question: if we're so eager to

gdb/breakpoint.c
@@ -12622,6 +12622,9 @@ notify_breakpoint_deleted (breakpoint *b)
 void
 delete_breakpoint (struct breakpoint *bpt)
 {
+  /* Make sure that the function cannot be interrupted by QUIT.  */
+  scoped_suppress_quit do_scoped_suppress_quit;
+
   gdb_assert (bpt != NULL);
 
   /* Has this bp already been deleted?  This can happen because

gdb/event-top.c
@@ -1202,6 +1202,9 @@ maybe_quit ()
   if (!is_main_thread ())
     return;
 
+  if (scoped_suppress_quit::suppress_quit_enabled ())
+    return;
+
   if (sync_quit_force_run)
     quit ();
 

gdb/utils.c
@@ -655,6 +655,48 @@ warning_filename_and_errno (const char *filename, int saved_errno)
 	   safe_strerror (saved_errno));
 }
 
+/* See utils.h.  */
+
+int scoped_suppress_quit::suppress_quit_enabled_cnt;
+bool scoped_suppress_quit::suppress_quit_seen;
+
+/* See utils.h.  */
+
+scoped_suppress_quit::scoped_suppress_quit ()
+{
+  gdb_assert (is_main_thread ());
+  suppress_quit_enabled_cnt++;
+}
+
+/* See utils.h.  */
+
+scoped_suppress_quit::~scoped_suppress_quit () noexcept(false)
+{
+  suppress_quit_enabled_cnt--;
+  gdb_assert (suppress_quit_enabled_cnt >= 0);
+
+  if (suppress_quit_enabled_cnt != 0)
+    return;
+
+  bool tmp_suppress_quit_seen = suppress_quit_seen;
+  suppress_quit_seen = false;
+
+  if (!tmp_suppress_quit_seen)
+    return;
+
+  /* A QUIT was suppressed, unsuppress it here.  This may throw.  */
+  QUIT;
+}
+
+/* See utils.h.  */
+
+bool
+scoped_suppress_quit::suppress_quit_enabled ()
+{
+  suppress_quit_seen = true;
+  return suppress_quit_enabled_cnt > 0;
+}
+
 /* Called when a memory allocation fails, with the number of bytes of
    memory requested in SIZE.  */
 

gdb/utils.h
@@ -486,4 +486,25 @@ private:
   std::vector<string_file> m_warnings;
 };
 
+/* RAII-style class to suppress the effect of calling QUIT until scope exit.  */
+
+class scoped_suppress_quit
+{
+public:
+
+  scoped_suppress_quit ();
+  ~scoped_suppress_quit () noexcept(false);
+
+  /* Whether suppression is currently active.  */
+  static bool suppress_quit_enabled ();
+
+private:
+
+  /* Nesting level.  */
+  static int suppress_quit_enabled_cnt;
+
+  /* Whether QUIT was called during suppression.  */
+  static bool suppress_quit_seen;
+};
+
 #endif /* GDB_UTILS_H */