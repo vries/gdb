vries/gdb

commit 817003ed46963e6f8e0782a2e709a9cfde71b0d9

Rewrite output redirection and logging
This patch changes how gdb output redirection is done.

Currently, output is done via the UI.  gdb_stdout, for example, is a
define the expands to an lvalue referencing a field in the current UI.
When redirecting, this field may temporarily be reset; and when
logging is enabled or disabled, this is also done.

This has lead to bugs where the combination of redirection and logging
results in use-after-free.  Crashes are readily observable; see the
new test cases.

This patch upends this.  Now, gdb_stdout is simply an rvalue, and
refers to the current interpreter.  The interpreter provides ui_files
that do whatever rewriting is needed (mostly for MI); then output is
forward to the current UI via an indirection (see the new
ui::passthrough_file).

The ui provides paging, logging, timestamps, and the final stream that
writes to an actual file descriptor.

Redirection is handled at the ui layer.  Rather than changing the
output pipeline, new ui_files are simply swapped in by rewriting
pointers, hopefully with a scoped_restore.

Redirecting at the ui layer means that interpreter rewriting is still
applied when capturing output.  This fixes one of the reported bugs.

Not changing the pipeline means that the problems with the combination
of redirect and logging simply vanish.  Logging just changes a flag
and doesn't involve object destruction.

Bug: https://sourceware.org/bugzilla/show_bug.cgi?id=17697
Bug: https://sourceware.org/bugzilla/show_bug.cgi?id=28620
Bug: https://sourceware.org/bugzilla/show_bug.cgi?id=28798
Bug: https://sourceware.org/bugzilla/show_bug.cgi?id=28948
Approved-By: Andrew Burgess <aburgess@redhat.com>

gdb/ui.h
@@ -22,8 +22,10 @@
 #include "gdbsupport/intrusive_list.h"
 #include "gdbsupport/next-iterator.h"
 #include "gdbsupport/scoped_restore.h"
+#include "ui-file.h"
 
 struct interp;
+struct ui;
 
 /* Prompt state.  */
 
@@ -42,6 +44,17 @@ enum prompt_state
   /* We've displayed the prompt already, ready for input.  */
   PROMPTED,
 };
+
+/* The main UI.  This is the UI that is bound to stdin/stdout/stderr.
+   It always exists and is created automatically when GDB starts
+   up.  */
+extern struct ui *main_ui;
+
+/* The current UI.  */
+extern struct ui *current_ui;
+
+/* The list of all UIs.  */
+extern struct ui *ui_list;
 
 /* All about a user interface instance.  Each user interface has its
    own I/O files/streams, readline state, its own top level
@@ -141,22 +154,60 @@ struct ui
      execution.  */
   bool keep_prompt_blocked = false;
 
+  /* A "smart pointer" that references a particular member of the
+     current UI.  */
+  template<ui_file *ui::* F>
+  struct ui_file_ptr
+  {
+    ui_file *operator-> () const
+    {
+      return current_ui->*F;
+    }
+  };
+
+  /* A ui_file that simply forwards.  */
+  template<ui_file *ui::* F>
+  class passthrough_file : public wrapped_file<ui_file_ptr<F>>
+  {
+  public:
+    passthrough_file () : wrapped_file<ui_file_ptr<F>> ({})
+    {
+    }
+
+    void write (const char *buf, long len) override
+    {
+      this->m_stream->write (buf, len);
+    }
+  };
+
   /* The fields below that start with "m_" are "private".  They're
      meant to be accessed through wrapper macros that make them look
      like globals.  */
 
   /* The ui_file streams.  */
   /* Normal results */
-  struct ui_file *m_gdb_stdout;
+  struct ui_file *m_ui_stdout;
   /* Input stream */
   struct ui_file *m_gdb_stdin;
   /* Serious error notifications */
-  struct ui_file *m_gdb_stderr;
+  struct ui_file *m_ui_stderr;
   /* Log/debug/trace messages that should bypass normal stdout/stderr
      filtering.  */
-  struct ui_file *m_gdb_stdlog;
+  struct ui_file *m_ui_stdlog;
   /* Target output.  */
-  struct ui_file *m_gdb_stdtarg;
+  struct ui_file *m_ui_stdtarg;
+
+  /* Types for directing output to the various UI-managed streams.  An
+     indirection solves all problems.  Here the problem being solved
+     is that an interpreter could be active with any UI; and that
+     redirections (like capturing command output to a string) is done
+     by manipulating the pointers in the current UI.  To this end, we
+     export some ui_file types that can be used to implement the
+     various kinds of redirections.  */
+  using ui_stdout_file = passthrough_file<&ui::m_ui_stdout>;
+  using ui_stderr_file = passthrough_file<&ui::m_ui_stderr>;
+  using ui_stdlog_file = passthrough_file<&ui::m_ui_stdlog>;
+  using ui_stdtarg_file = passthrough_file<&ui::m_ui_stdtarg>;
 
   /* The current ui_out.  */
   struct ui_out *m_current_uiout = nullptr;
@@ -170,17 +221,6 @@ struct ui
   /* Return true if this UI's input fd is a tty.  */
   bool input_interactive_p () const;
 };
-
-/* The main UI.  This is the UI that is bound to stdin/stdout/stderr.
-   It always exists and is created automatically when GDB starts
-   up.  */
-extern struct ui *main_ui;
-
-/* The current UI.  */
-extern struct ui *current_ui;
-
-/* The list of all UIs.  */
-extern struct ui *ui_list;
 
 /* State for SWITCH_THRU_ALL_UIS.  */
 class switch_thru_all_uis

gdb/buffered-streams.c
@@ -89,16 +89,16 @@ get_unbuffered (ui_file *stream)
 }
 
 buffered_streams::buffered_streams (buffer_group *group, ui_out *uiout)
-  : m_buffered_stdout (group, gdb_stdout),
-    m_buffered_stderr (group, gdb_stderr),
-    m_buffered_stdlog (group, gdb_stdlog),
-    m_buffered_stdtarg (group, gdb_stdtarg),
+  : m_buffered_stdout (group, *redirectable_stdout ()),
+    m_buffered_stderr (group, *redirectable_stderr ()),
+    m_buffered_stdlog (group, *redirectable_stdlog ()),
+    m_buffered_stdtarg (group, *redirectable_stdtarg ()),
     m_uiout (uiout)
 {
-  gdb_stdout = &m_buffered_stdout;
-  gdb_stderr = &m_buffered_stderr;
-  gdb_stdlog = &m_buffered_stdlog;
-  gdb_stdtarg = &m_buffered_stdtarg;
+  *redirectable_stdout () = &m_buffered_stdout;
+  *redirectable_stderr () = &m_buffered_stderr;
+  *redirectable_stdlog () = &m_buffered_stdlog;
+  *redirectable_stdtarg () = &m_buffered_stdtarg;
 
   ui_file *stream = current_uiout->current_stream ();
   if (stream != nullptr)
@@ -127,10 +127,10 @@ buffered_streams::remove_buffers ()
 
   m_buffers_in_place = false;
 
-  gdb_stdout = m_buffered_stdout.stream ();
-  gdb_stderr = m_buffered_stderr.stream ();
-  gdb_stdlog = m_buffered_stdlog.stream ();
-  gdb_stdtarg = m_buffered_stdtarg.stream ();
+  *redirectable_stdout () = m_buffered_stdout.stream ();
+  *redirectable_stderr () = m_buffered_stderr.stream ();
+  *redirectable_stdlog () = m_buffered_stdlog.stream ();
+  *redirectable_stdtarg () = m_buffered_stdtarg.stream ();
 
   if (m_buffered_current_uiout.has_value ())
     current_uiout->redirect (nullptr);

gdb/cli/cli-interp.c
@@ -59,7 +59,7 @@ private:
 
 cli_interp::cli_interp (const char *name)
   : cli_interp_base (name),
-    m_cli_uiout (new cli_ui_out (gdb_stdout))
+    m_cli_uiout (new cli_ui_out (m_stdout.get ()))
 {
 }
 
@@ -183,27 +183,10 @@ void
 cli_interp::resume ()
 {
   struct ui *ui = current_ui;
-  struct ui_file *stream;
-
-  /*sync_execution = 1; */
-
-  /* gdb_setup_readline will change gdb_stdout.  If the CLI was
-     previously writing to gdb_stdout, then set it to the new
-     gdb_stdout afterwards.  */
-
-  stream = m_cli_uiout->set_stream (gdb_stdout);
-  if (stream != gdb_stdout)
-    {
-      m_cli_uiout->set_stream (stream);
-      stream = NULL;
-    }
 
   gdb_setup_readline (1);
 
   ui->input_handler = command_line_handler;
-
-  if (stream != NULL)
-    m_cli_uiout->set_stream (gdb_stdout);
 }
 
 void
@@ -250,57 +233,6 @@ ui_out *
 cli_interp::interp_ui_out ()
 {
   return m_cli_uiout.get ();
-}
-
-/* See cli-interp.h.  */
-
-void
-cli_interp_base::set_logging (ui_file_up logfile, bool logging_redirect,
-			      bool debug_redirect)
-{
-  if (logfile != nullptr)
-    {
-      gdb_assert (m_saved_output == nullptr);
-      m_saved_output = std::make_unique<saved_output_files> ();
-      m_saved_output->out = gdb_stdout;
-      m_saved_output->err = gdb_stderr;
-      m_saved_output->log = gdb_stdlog;
-      m_saved_output->targ = gdb_stdtarg;
-
-      ui_file *logfile_p = logfile.get ();
-      m_saved_output->logfile_holder = std::move (logfile);
-
-      /* The new stdout and stderr only depend on whether logging
-	 redirection is being done.  */
-      ui_file *new_stdout = logfile_p;
-      ui_file *new_stderr = logfile_p;
-      if (!logging_redirect)
-	{
-	  m_saved_output->stdout_holder.reset
-	    (new tee_file (gdb_stdout, logfile_p));
-	  new_stdout = m_saved_output->stdout_holder.get ();
-	  m_saved_output->stderr_holder.reset
-	    (new tee_file (gdb_stderr, logfile_p));
-	  new_stderr = m_saved_output->stderr_holder.get ();
-	}
-
-      m_saved_output->stdlog_holder.reset
-	(new timestamped_file (debug_redirect ? logfile_p : new_stderr));
-
-      gdb_stdout = new_stdout;
-      gdb_stdlog = m_saved_output->stdlog_holder.get ();
-      gdb_stderr = new_stderr;
-      gdb_stdtarg = new_stderr;
-    }
-  else
-    {
-      gdb_stdout = m_saved_output->out;
-      gdb_stderr = m_saved_output->err;
-      gdb_stdlog = m_saved_output->log;
-      gdb_stdtarg = m_saved_output->targ;
-
-      m_saved_output.reset (nullptr);
-    }
 }
 
 /* Factory for CLI interpreters.  */

gdb/cli/cli-interp.h
@@ -28,8 +28,6 @@ public:
   explicit cli_interp_base (const char *name);
   virtual ~cli_interp_base () = 0;
 
-  void set_logging (ui_file_up logfile, bool logging_redirect,
-		    bool debug_redirect) override;
   void pre_command_loop () override;
   bool supports_command_editing () override;
 
@@ -41,26 +39,6 @@ public:
   void on_sync_execution_done () override;
   void on_command_error () override;
   void on_user_selected_context_changed (user_selected_what selection) override;
-
-private:
-  struct saved_output_files
-  {
-    /* Saved gdb_stdout, gdb_stderr, etc.  */
-    ui_file *out;
-    ui_file *err;
-    ui_file *log;
-    ui_file *targ;
-    /* When redirecting, some or all of these may be non-null
-       depending on the logging mode.  */
-    ui_file_up stdout_holder;
-    ui_file_up stderr_holder;
-    ui_file_up stdlog_holder;
-    ui_file_up logfile_holder;
-  };
-
-  /* These hold the pushed copies of the gdb output files.  If NULL
-     then nothing has yet been pushed.  */
-  std::unique_ptr<saved_output_files> m_saved_output;
 };
 
 /* Returns true if the current stop should be printed to

gdb/cli/cli-logging.c
@@ -213,17 +213,6 @@ logging_file<T>::puts_unfiltered (const char *str)
 template class logging_file<ui_file *>;
 template class logging_file<ui_file_up>;
 
-/* If we've pushed output files, close them and pop them.  */
-static void
-pop_output_files (void)
-{
-  current_interp_set_logging (NULL, false, false);
-
-  /* Stay consistent with handle_redirections.  */
-  if (!current_uiout->is_mi_like_p ())
-    current_uiout->redirect (NULL);
-}
-
 /* This is a helper for the `set logging' command.  */
 static void
 handle_redirections (int from_tty)
@@ -260,19 +249,7 @@ handle_redirections (int from_tty)
   saved_filename = logging_filename;
   logging_redirect_for_file = logging_redirect;
   debug_redirect_for_file = debug_redirect;
-
-  /* Let the interpreter do anything it needs.  */
-  current_interp_set_logging (std::move (log), logging_redirect,
-			      debug_redirect);
-
-  /* Redirect the current ui-out object's output to the log.  Use
-     gdb_stdout, not log, since the interpreter may have created a tee
-     that wraps the log.  Don't do the redirect for MI, it confuses
-     MI's ui-out scheme.  Note that we may get here with MI as current
-     interpreter, but with the current ui_out as a CLI ui_out, with
-     '-interpreter-exec console "set logging on"'.  */
-  if (!current_uiout->is_mi_like_p ())
-    current_uiout->redirect (gdb_stdout);
+  log_file = std::move (log);
 }
 
 static void
@@ -292,7 +269,7 @@ set_logging_off (const char *args, int from_tty)
   if (saved_filename.empty ())
     return;
 
-  pop_output_files ();
+  log_file.reset ();
   if (from_tty)
     gdb_printf ("Done logging to %s.\n",
 		saved_filename.c_str ());

gdb/fork-child.c
@@ -47,8 +47,8 @@ get_exec_wrapper ()
 void
 gdb_flush_out_err ()
 {
-  gdb_flush (main_ui->m_gdb_stdout);
-  gdb_flush (main_ui->m_gdb_stderr);
+  gdb_flush (main_ui->m_ui_stdout);
+  gdb_flush (main_ui->m_ui_stderr);
 }
 
 /* The ui structure that will be saved on 'prefork_hook' and

gdb/guile/scm-ports.c
@@ -25,6 +25,7 @@
 #include "ui.h"
 #include "target.h"
 #include "guile-internal.h"
+#include "logging-file.h"
 #include <optional>
 
 #ifdef HAVE_POLL
@@ -323,20 +324,21 @@ ioscm_with_output_to_port_worker (SCM port, SCM thunk, enum oport oport,
 
   scoped_restore restore_async = make_scoped_restore (&current_ui->async, 0);
 
-  ui_file_up port_file (new ioscm_file_port (port));
+  ui_file_up port_file = std::make_unique<ioscm_file_port> (port);
 
   scoped_restore save_file = make_scoped_restore (oport == GDB_STDERR
-						  ? &gdb_stderr : &gdb_stdout);
+						  ? redirectable_stderr ()
+						  : redirectable_stdout ());
 
   {
     std::optional<ui_out_redirect_pop> redirect_popper;
     if (oport == GDB_STDERR)
-      gdb_stderr = port_file.get ();
+      *redirectable_stderr () = port_file.get ();
     else
       {
 	redirect_popper.emplace (current_uiout, port_file.get ());
 
-	gdb_stdout = port_file.get ();
+	*redirectable_stderr () = port_file.get ();
       }
 
     result = gdbscm_safe_call_0 (thunk, NULL);

gdb/interps.c
@@ -45,9 +45,16 @@
 static struct interp *interp_lookup_existing (struct ui *ui,
 					      const char *name);
 
-interp::interp (const char *name)
+interp::interp (const char *name, bool make_outputs)
   : m_name (name)
 {
+  if (make_outputs)
+    {
+      m_stdout = std::make_unique<ui::ui_stdout_file> ();
+      m_stderr = std::make_unique<ui::ui_stderr_file> ();
+      m_stdlog = std::make_unique<ui::ui_stdlog_file> ();
+      m_stdtarg = std::make_unique<ui::ui_stdtarg_file> ();
+    }
 }
 
 interp::~interp () = default;
@@ -197,15 +204,6 @@ set_top_level_interpreter (const char *name, bool for_new_ui)
   interp_set (interp, true);
 }
 
-void
-current_interp_set_logging (ui_file_up logfile, bool logging_redirect,
-			    bool debug_redirect)
-{
-  struct interp *interp = current_ui->current_interpreter;
-
-  interp->set_logging (std::move (logfile), logging_redirect, debug_redirect);
-}
-
 /* Temporarily overrides the current interpreter.  */
 struct interp *
 scoped_restore_interp::set_interp (const char *name)
@@ -285,13 +283,6 @@ interpreter_exec_cmd (const char *args, int from_tty)
   struct interp *interp_to_use;
   unsigned int nrules;
   unsigned int i;
-
-  /* Interpreters may clobber stdout/stderr (e.g.  in mi_interp::resume at time
-     of writing), preserve their state here.  */
-  scoped_restore save_stdout = make_scoped_restore (&gdb_stdout);
-  scoped_restore save_stderr = make_scoped_restore (&gdb_stderr);
-  scoped_restore save_stdlog = make_scoped_restore (&gdb_stdlog);
-  scoped_restore save_stdtarg = make_scoped_restore (&gdb_stdtarg);
 
   if (args == NULL)
     error_no_arg (_("interpreter-exec command"));

commit b6d75209306f3741269e8927f3a350318562fff9

[gdb] Fix segfault in sig_write
I ran into a segfault in sig_write:
...
  if (gdb_stderr == nullptr || gdb_stderr->fd () == -1)
...

[ A regression since commit 817003ed469 ("Rewrite output redirection and
logging"). ]

This happens as follows.

First, we get gdb_stderr by calling current_gdb_stderr, which returns
current_ui.current_interpreter.m_stderr.get (), which is not nullptr.

Then we do "gdb_stderr->fd ()", which gets us to
wrapped_file<ui::ui_file_ptr<&ui::m_ui_stderr> >::fd:
...
  int fd () const override
  { return m_stream->fd (); }
...

The "m_stream->" part brings us to:
...
  /* A "smart pointer" that references a particular member of the
     current UI.  */
  template<ui_file *ui::* F>
  struct ui_file_ptr
  {
    ui_file *operator-> () const
    {
      return current_ui->*F;
    }
  };
...
which does "current_ui.m_ui_stderr" which indeed is nullptr.

Fix this in wrapped_file::fd by checking for a nullptr m_stream.

A v1 was submitted here [1].

Changes in v2:
- changed approach of fixing this: rather than trying to fix this in sig_write
  by avoiding the use of gdb_stderr, fix this in wrapped_file::fd.
- added unit test

Tested on x86_64-linux.

Bug: https://sourceware.org/bugzilla/show_bug.cgi?id=33918

[1] v1: https://sourceware.org/pipermail/gdb-patches/2026-February/225155.html

gdb/ui-file.h
@@ -420,7 +420,7 @@ public:
   { m_stream->emit_style_escape (style); }
 
   int fd () const override
-  { return m_stream->fd (); }
+  { return m_stream == nullptr ? -1 : m_stream->fd (); }
 
   void puts_unfiltered (const char *str) override
   { m_stream->puts_unfiltered (str); }

gdb/ui.h
@@ -163,6 +163,10 @@ struct ui
     {
       return current_ui->*F;
     }
+    bool operator== (nullptr_t p) const
+    {
+      return current_ui->*F == nullptr;
+    }
   };
 
   /* A ui_file that simply forwards.  */

gdb/unittests/ui-file-selftests.c
@@ -48,6 +48,20 @@ run_tests ()
   scoped_restore save_7 = make_scoped_restore (&sevenbit_strings, true);
   check_one ("more weird stuff: \xa5", '\\',
 	     "more weird stuff: \\245");
+
+  {
+    /* There's a bug that has the effect "*redirectable_stderr () == nullptr".
+       In that case, gdb_stderr is not nullptr, but the underlying pointer is
+       a nullptr.  Check that "gdb_stderr->fd ()" doesn't dereference the
+       underlying nullptr.
+       This allows us to check for a usable gdb_stderr using
+       "gdb_stderr != nullptr && gdb_stderr->fd () == -1", as we do in
+       sig_write.  */
+    scoped_restore restore_stderr
+      = make_scoped_restore (redirectable_stderr (), nullptr);
+    SELF_CHECK (gdb_stderr != nullptr);
+    SELF_CHECK (gdb_stderr->fd () == -1);
+  }
 }
 
 } /* namespace file*/