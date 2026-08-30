dcd39364fd8dda1a4cef2a9b7a5417ca57e1d801
Tom de Vries <tdevries@suse.de>
2026-08-30 06:48:58 +0200

    [gdb] Make gdb_buildall.sh shellcheck-clean
    Make gdb_buildall.sh shellcheck-clean:
    - add missing quoting
    - use $()
    - use bash instead of sh to allow non-POSIX features
    - use makejobs="$1" instead of makejobs="-j $1"
    - use array for targexp
    - use read -r
    - add missing definition of gdbopts and simopts
    - use '' for trap argument
    - remove useless cat

--- gdb/contrib/shellcheck.sh
+++ gdb/contrib/shellcheck.sh
@@ -45,7 +45,6 @@ for f in "$@"; do
 	    | gdb/config/djgpp/djconfig.sh \
 	    | gdb/contrib/cc-with-tweaks.sh \
 	    | gdb/contrib/gdb-add-index.sh \
-	    | gdb/gdb_buildall.sh \
 	    | gdb/gdb_mbuild.sh \
 	    | gdb/testsuite/lib/pdtrace.in)
 	    # Skip unclean files.
