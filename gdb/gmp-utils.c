2a9afbf1814e6464d0844a4cd491320d82c3b63c
Tom de Vries <tdevries@suse.de>
2026-09-10 07:53:10 +0200

    Revert "Rewrite gdb_mpz::export_bits"
    This reverts commit 69c5e3114ecf4056d56a0feeead3fb2a64952376.

--- gdb/gmp-utils.c
+++ gdb/gmp-utils.c
@@ -128,22 +128,35 @@ gdb_mpz::export_bits (gdb::array_view<gdb_byte> buf, int endian, bool unsigned_p
 	       hi.str ().c_str ());
     }
 
-  gdb_mpz masked = *this;
-  masked.mask (buf.size () * HOST_CHAR_BIT);
-
-  if (sign < 0 && masked.sgn () != 0)
+  const gdb_mpz *exported_val = this;
+  gdb_mpz un_signed;
+  if (sign < 0)
     {
       /* mpz_export does not handle signed values, so create a positive
 	 value whose bit representation as an unsigned of the same length
-	 would be the same as our negative value.  However, if masking
-	 left us with 0, we don't need to do anything else.  */
+	 would be the same as our negative value.  */
       gdb_mpz neg_offset = gdb_mpz::pow (2, buf.size () * HOST_CHAR_BIT);
-      masked += neg_offset;
+      un_signed = *exported_val + neg_offset;
+      exported_val = &un_signed;
     }
 
-  /* It's possible that the above results in zero, which has to be
-     handled specially.  */
-  if (masked.sgn () == 0)
+  /* If the value is too large, truncate it.  */
+  if (!safe
+      && mpz_sizeinbase (exported_val->m_val, 2) > buf.size () * HOST_CHAR_BIT)
+    {
+      /* If we don't already have a copy, make it now.  */
+      if (exported_val != &un_signed)
+	{
+	  un_signed = *exported_val;
+	  exported_val = &un_signed;
+	}
+
+      un_signed.mask (buf.size () * HOST_CHAR_BIT);
+    }
+
+  /* It's possible that one of the above results in zero, which has to
+     be handled specially.  */
+  if (exported_val->sgn () == 0)
     {
       memset (buf.data (), 0, buf.size ());
       return;
@@ -161,9 +174,8 @@ gdb_mpz::export_bits (gdb::array_view<gdb_byte> buf, int endian, bool unsigned_p
 
   size_t word_countp;
   gdb::unique_xmalloc_ptr<void> exported
-    (mpz_export (nullptr, &word_countp, -1 /* order */,
-		 buf.size () /* size */, endian, 0 /* nails */,
-		 masked.m_val));
+    (mpz_export (NULL, &word_countp, -1 /* order */, buf.size () /* size */,
+		 endian, 0 /* nails */, exported_val->m_val));
 
   gdb_assert (word_countp == 1);
 
